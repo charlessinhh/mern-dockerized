414d3b7b7be54e22a03c71c0fc892d888119a2ed
CHARLES KUMAR SINGH <39948804+charlessinhh@users.noreply.github.com>
2023-05-09 19:50:09 +0530

    Update AddProduct.js

--- frontend/src/components/AddProduct.js
+++ frontend/src/components/AddProduct.js
@@ -21,7 +21,7 @@ const AddProduct = () => {
     const userId = user._id;
     console.log(userId);
 
-    let productData = await fetch("http://backend-api:5000/add-product", {
+    let productData = await fetch("/add-product", {
       method: "post",
       body: JSON.stringify({ name, price, category, company, userId }),
       headers: {
